8947f05e7a332ec506892f11991483a7e67cc6d1
Fadzril Muhamad <fadzril@jomcode.com>
2012-08-09 02:57:23 +0800

    change click stream graph to provide label, discard previous implementation

--- app/views/home_view.js
+++ app/views/home_view.js
@@ -23,70 +23,98 @@ module.exports = App.Views.Home = Em.View.extend({
     build: function() {
         var m = [20, 120, 20, 120],
             width = 1080 - m[1] - m[3],
-            height = 600 - m[0] - m[2],   
+            height = 600 - m[0] - m[2],
+            focalNodeID = 0,
             totalClick = 0,
+            centerNodeSize = 50,
+            nodeSize = 10,
             _node, _links;
         var color = d3.scale.category20();
 
         var force = d3.layout.force()
-            .charge(-350)
-            .linkDistance(60)
+            .charge(-450)
+            .linkDistance(100)
             .size([width, height]);
 
-        var svg = d3.select("#chart").append("svg")
+        var svg = d3.select("#chart").append("svg:svg")
             .attr("width", width)
             .attr("height", height);
 
         d3.json("miserables3.json", function(json) {
+            var nodeLinks = json.links,
+                nodeInst = json.nodes;
+
             force.nodes(json.nodes)
                 .links(json.links)
+                .on('tick', tick)
                 .start();
 
-            _node = json.nodes;
-            _links = json.links;
-            var link = svg.selectAll("line.link")
-                .data(json.links)
-                .enter().append("line")
-                .attr("class",function(d){return d.isingle == 'yes' ? "center":"link";})
-                .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-            
-            link.append('title')
-              .text(function(d) {return d.value; });
-            
-            for (var index = 0 ; index <json.links.length; index++){
-                totalClick += json.links[index].value;
-            };
-              
-            var node = svg.selectAll("circle.node")
-                .data(json.nodes)
-                .enter().append("circle")
-                .attr("class", "node")
-                .attr("r", 5)
+            // Per-type markers, as they don't inherit styles.
+            svg.append("svg:defs").selectAll("marker")
+                .data(force.links())
+              .enter().append("svg:marker")
+                .attr("id", String)
+                .attr("viewBox", "0 -5 10 10")
+                .attr("refX", 15)
+                .attr("refY", -1.5)
+                .attr("markerWidth", 6)
+                .attr("markerHeight", 6)
+                .attr("orient", "auto")
+              .append("svg:path")
+                .attr("d", "M0,-5L10,0L0,5");
+
+            var path = svg.append("svg:g").selectAll("path")
+                .data(force.links())
+              .enter().append("svg:path")
+                .attr("class", function(d) { return "link " + d.group; })
+                .attr("marker-end", function(d) { return "url(#" + d.group + ")"; });
+
+            var circle = svg.append("svg:g").selectAll("circle")
+                .data(force.nodes())
+              .enter().append("svg:circle")
+                .attr("r", 6)
                 .style("fill", function(d) { return color(d.group); })
                 .call(force.drag);
 
-            node.append("svg:text")
-                .attr("class", "node")
-                .attr("x", function(d) { return d.x; })
-                .attr("y", function(d) { return d.y; })
-                .text(function(d) { return d.name; })
+            var text = svg.append("svg:g").selectAll("g")
+                .data(force.nodes())
+              .enter().append("svg:g");
 
-            node.append('title')
-                .text(function(d) { return d.name });
+            // A copy of the text with a thick white stroke for legibility.
+            text.append("svg:text")
+                .attr("x", 8)
+                .attr("y", ".31em")
+                .attr("class", "shadow")
+                .style("fill", "#333")
+                .text(function(d) { return d.name; });
 
-            force.on("tick", function() {
-                link.attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
+            text.append("svg:text")
+                .attr("x", 8)
+                .attr("y", ".31em")
+                .style("fill", "#999")
+                .text(function(d) { return d.name; });
 
-            node.attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
-            });
+            // Use elliptical arc path segments to doubly-encode directionality.
+            function tick() {
+                path.attr("d", function(d) {
+                    var dx = d.target.x - d.source.x,
+                        dy = d.target.y - d.source.y,
+                        dr = Math.sqrt(dx * dx + dy * dy);
+                        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+                    });
 
-            node.on('click',function(d){
-                var source = _(_(_links).pluck('source')).pluck('index'),
-                    target = _(_(_links).pluck('target')).pluck('index'),
+                    circle.attr("transform", function(d) {
+                        return "translate(" + d.x + "," + d.y + ")";
+                    });
+
+                    text.attr("transform", function(d) {
+                        return "translate(" + d.x + "," + d.y + ")";
+                    });
+            }
+
+            circle.on('click', function(d) {
+                var source = _(_(nodeLinks).pluck('source')).pluck('index'),
+                    target = _(_(nodeLinks).pluck('target')).pluck('index'),
                     _targetIndex = [], _sourceIndex = [], obj = [];
 
                 _(target).each(function(val,index){
@@ -94,9 +122,9 @@ module.exports = App.Views.Home = Em.View.extend({
                         _targetIndex.push(index);
                         var _obj = {
                                 port: "port",
-                                octetTotalCount: _links[index].value,
-                                name : _links[index].source.name,
-                                color: color(_links[index].source.group)
+                                octetTotalCount: nodeLinks[index].value,
+                                name : nodeLinks[index].source.name,
+                                color: color(nodeLinks[index].source.group)
                             };
                         obj.push(_obj);
                     }
@@ -106,9 +134,9 @@ module.exports = App.Views.Home = Em.View.extend({
                         _sourceIndex.push(index);
                         var _obj = {
                                 port: "port",
-                                octetTotalCount: _links[index].value,
-                                name : _links[index].target.name,
-                                color:color(_links[index].target.group)
+                                octetTotalCount: nodeLinks[index].value,
+                                name : nodeLinks[index].target.name,
+                                color:color(nodeLinks[index].target.group)
 
                             };
                         obj.push(_obj);
